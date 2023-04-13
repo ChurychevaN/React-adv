8fcc244056be733dfeb6dafe92b001506520a284
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-04-13 14:23:47 +0200

    ID-selected-event-on-the-EventDetailPage

--- frontend/src/page/Events.js
+++ frontend/src/page/Events.js
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
 
 const DUMMY_LIST = [
-	{id: 'e1', title: 'Event 1'},
-	{id: 'e2', title: 'Event 2'},
-	{id: 'e3', title: 'Event 3'},
-	{id: 'e4', title: 'Event 4'},
-	{id: 'e5', title: 'Event 5'},
-]
+	{ id: 'e1', title: 'Event 1' },
+	{ id: 'e2', title: 'Event 2' },
+	{ id: 'e3', title: 'Event 3' },
+	{ id: 'e4', title: 'Event 4' },
+	{ id: 'e5', title: 'Event 5' },
+];
 
 function EventsPage() {
 	return (
 		<>
 			<h1>Events Page</h1>
-			<ul>{DUMMY_LIST.map(ev => <li key={ev.id}><Link to={ev.id}>{ev.title}</Link></li>)}</ul>
+			<ul>{ DUMMY_LIST.map(
+				ev => <li key={ ev.id }><Link to={ ev.id }>{ ev.title }</Link>
+				</li>) }</ul>
 		</>
-	)
+	);
 }
 
 export default EventsPage;
