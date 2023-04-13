1da6c07a2816a027c38687814f67b160f0b70c1d
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-04-13 13:58:36 +0200

    list-of-dummy-events-to-the-EventsPage

--- frontend/src/page/Events.js
+++ frontend/src/page/Events.js
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_LIST = [
+	{id: 'e1', title: 'Event 1'},
+	{id: 'e2', title: 'Event 2'},
+	{id: 'e3', title: 'Event 3'},
+	{id: 'e4', title: 'Event 4'},
+	{id: 'e5', title: 'Event 5'},
+]
+
 function EventsPage() {
-	return <h1>Events Page</h1>
+	return (
+		<>
+			<h1>Events Page</h1>
+			<ul>{DUMMY_LIST.map(ev => <li key={ev.id}><Link to={ev.id}>{ev.title}</Link></li>)}</ul>
+		</>
+	)
 }
 
 export default EventsPage;
