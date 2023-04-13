af60b584e1ec3befe65c247b61ad480f5c8f363b
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-04-13 12:24:26 +0200

    Add-routing

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,20 +1,22 @@
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={ classes.header }>
+			<main>
+				<nav>
+					<ul className={ classes.list }>
+						<li>
+							<a>Home</a>
+						</li>
+						<li>
+							<a>Events</a>
+						</li>
+					</ul>
+				</nav>
+			</main>
+		</header>
+	);
 }
 
 export default MainNavigation;
