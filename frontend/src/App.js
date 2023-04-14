7fb6c88c02a86ce113e97f6d40aa4b7c8f82ee88
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-04-14 14:51:18 +0200

    error-handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,14 @@ import NewEventPage from './page/NewEvent';
 import EditEventPage from './page/EditEvent';
 import RootLayout from './page/Root';
 import RootLayoutPage from './page/RootLayout';
+import ErrorPage from './page/Error';
 
 const router = createBrowserRouter([
 	{
-		path: '/', element: <RootLayout />, children: [
+		path: '/',
+		element: <RootLayout />,
+		errorElement: <ErrorPage />,
+		children: [
 			{ index: true, element: <HomePage /> },
 			{
 				path: 'events', element: <RootLayoutPage />, children: [
--- frontend/src/page/Events.js
+++ frontend/src/page/Events.js
@@ -1,20 +1,27 @@
-import {useLoaderData} from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-const events = useLoaderData();
+	const data = useLoaderData();
+
+	// if (data.isError) {
+	// 	return <p>{ data.message }</p>;
+	// }
+	const events = data.events;
 	return (
-			<EventsList events={events} />
+		<EventsList events={ events } />
 	);
 }
 
 export default EventsPage;
 
 export async function loader() {
-		const response = await fetch('http://localhost:8080/events');
+	const response = await fetch('http://localhost:8080/eventsfffffffffffffffffffff');
 
-		if (!response.ok) {
-		} else {
-			return response;
-		}
+	if (!response.ok) {
+		// return { isError: true, message: 'Could not fetch events.' };
+		throw {message: 'Could not fetch events!!!!!!!!!!'}
+	} else {
+		return response;
+	}
 }
