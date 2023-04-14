69e87d4d44c6c4d238096c32d5e475cab110b44e
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-04-14 15:31:23 +0200

    json-helper-function

--- frontend/src/page/Error.js
+++ frontend/src/page/Error.js
@@ -1,5 +1,29 @@
+import PageContent from '../components/PageContent';
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
 function ErrorPage() {
-	return <h1>Error Page</h1>
+	const error = useRouteError();
+	let title = 'An error occurred';
+	let message = 'Something wron!!';
+
+	if (error.state === 500) {
+		message = JSON.parse(error.data).message;
+	}
+
+	if (error.status === 400) {
+		title = 'Nod found!';
+		message = 'Could not find resource or page...';
+	}
+
+	return(
+	<>
+		<MainNavigation />
+	<PageContent title={ title }>
+		<p>{ message }</p>
+	</PageContent>;
+	</>
+	)
 }
 
 export default ErrorPage;
--- frontend/src/page/Events.js
+++ frontend/src/page/Events.js
@@ -4,9 +4,6 @@ import EventsList from '../components/EventsList';
 function EventsPage() {
 	const data = useLoaderData();
 
-	// if (data.isError) {
-	// 	return <p>{ data.message }</p>;
-	// }
 	const events = data.events;
 	return (
 		<EventsList events={ events } />
@@ -19,8 +16,9 @@ export async function loader() {
 	const response = await fetch('http://localhost:8080/eventsfffffffffffffffffffff');
 
 	if (!response.ok) {
-		// return { isError: true, message: 'Could not fetch events.' };
-		throw {message: 'Could not fetch events!!!!!!!!!!'}
+		throw new Response(JSON.stringify({message: 'Could not fetch events.........'}), {
+			status: 500,
+			})
 	} else {
 		return response;
 	}
